ff67f5cd5732f5d5ee359eda97a7e4e5d38b3bd7
NicolasPrietoVargas <nicolasprietovargas@gmail.com>
2025-06-25 16:30:46 -0500

    Terminado

--- src/main/java/com/edu/escuelaing/ARSW/controller/WebSiteController.java
+++ src/main/java/com/edu/escuelaing/ARSW/controller/WebSiteController.java
@@ -2,11 +2,8 @@ package com.edu.escuelaing.ARSW.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @SpringBootApplication
 @RestController
 public class WebSiteController {
@@ -24,7 +21,7 @@ public class WebSiteController {
                 + java.time.LocalTime.now()
                 + ". " + "The server is Runnig!\"}";
     }
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World")
                         String name) {
         return String.format("Hello %s!", name);
